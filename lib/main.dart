dc59296284d3dd942c6e6992b8d61417f12156c5
rtcteen <rtcteen30@gmail.com>
2024-12-21 16:00:02 +0700

    Create Container.

--- lib/main.dart
+++ lib/main.dart
@@ -12,29 +12,73 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'Flutter Demo',
+            style: TextStyle(color: Colors.white),
+          ),
+          leading: const Icon(Icons.add),
+          backgroundColor: Colors.purple,
+        ),
+        body: Container(
+          height: 100,
+          width: 100,
+          color: Colors.orange,
+          padding: const EdgeInsets.all(20),
+          margin: const EdgeInsets.all(20),
+          child: const Text('Hello')
+        ),
+          
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            print("Hello");
+          },
+          child: const Icon(Icons.phone),
+        ),
+    ));
   }
 }
+
+
+/* Week 1
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+            title: const Text("CPSU Project",
+                style: TextStyle(
+                    color: Colors.white, fontSize: 28, letterSpacing: 2)),
+            backgroundColor: Colors.redAccent,
+            centerTitle: true),
+        backgroundColor: Colors.pink,
+        body: Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+          Container(
+              padding: const EdgeInsets.all(15),
+              color: Colors.blueAccent,
+              child: const Text("Hello World1")),
+          Container(
+              padding: const EdgeInsets.all(15),
+              color: Colors.blueAccent,
+              child: const Text("Hello World2")),
+          Container(
+              padding: const EdgeInsets.all(15),
+              color: Colors.blueAccent,
+              child: const Text("Hello World3")),
+        ]));
+  }
+}
+
+*/
+
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
